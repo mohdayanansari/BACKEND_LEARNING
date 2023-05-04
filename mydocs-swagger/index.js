1cf66e131a2d507c6f0b1c3e23a5d3918dba7f2d
Mohd Ayan Ansari <mohdayanansari1999@gmail.com>
2023-05-04 23:34:39 +0530

    swagger

--- mydocs-swagger/index.js
+++ mydocs-swagger/index.js
@@ -1,20 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
-const YAML = require('yaml');
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const YAML = require("yaml");
 
-const file = fs.readFileSync('./swagger.yaml', 'utf8');
+const file = fs.readFileSync("./swagger.yaml", "utf8");
 const swaggerDocument = YAML.parse(file);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3050;
 
-app.get('/', (req, res) => {
-	res.send('Hello');
+const courses = [
+  {
+    id: "11",
+    name: "React Native",
+    price: 299,
+  },
+  {
+    id: "22",
+    name: "React JS",
+    price: 4899,
+  },
+  {
+    id: "33",
+    name: "Angular",
+    price: 499,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/api/v1/ayan", (req, res) => {
+  res.send("Hello from ayan");
+});
+app.get("/api/v1/object", (req, res) => {
+  res.send({
+    id: "55",
+    name: "learn backend",
+    price: 999,
+  });
+});
+app.get("/api/v1/array", (req, res) => {
+  res.send(courses);
+});
+app.get("/api/v1/course/:id", (req, res) => {
+  const myCourse = courses.find((c) => c.id === req.params.id);
+  res.status(200).send(myCourse);
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is running at ${PORT}🚀`);
+  console.log(`Server is running at ${PORT}🚀`);
 });
