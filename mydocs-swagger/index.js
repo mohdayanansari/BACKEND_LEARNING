c3576211ed2cb7ba0e1c18301a3ba2a4d3ba774d
Mohd Ayan Ansari <mohdayanansari1999@gmail.com>
2023-05-07 12:48:24 +0530

    commmit

--- mydocs-swagger/index.js
+++ mydocs-swagger/index.js
@@ -3,11 +3,14 @@ const app = express();
 const swaggerUi = require("swagger-ui-express");
 const fs = require("fs");
 const YAML = require("yaml");
+const fileUpload = require("express-fileupload");
 
 const file = fs.readFileSync("./swagger.yaml", "utf8");
 const swaggerDocument = YAML.parse(file);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+app.use(fileUpload());
 
 const PORT = process.env.PORT || 3050;
 
@@ -49,6 +52,35 @@ app.get("/api/v1/array", (req, res) => {
 app.get("/api/v1/course/:id", (req, res) => {
   const myCourse = courses.find((c) => c.id === req.params.id);
   res.status(200).send(myCourse);
+});
+
+app.post("/api/v1/addCourse", (req, res) => {
+  console.log(req.body);
+  courses.push(req.body);
+  res.send(true);
+});
+
+// handling query in swagger
+app.get("/api/v1/courseQuery", (req, res) => {
+  let location = req.query.location;
+  let device = req.query.device;
+
+  res.send({ location, device });
+});
+
+// uploading file
+app.post("/api/v1/upload", (req, res) => {
+  console.log(req.headers)
+  const file = req.files.file;
+  let path = __dirname + "/images/" + Date.now() + ".jpg";
+
+  file.mv(path, (err) => {
+    if (err) {
+      res.send(false);
+    } else {
+      res.send(true);
+    }
+  });
 });
 
 app.listen(PORT, () => {
