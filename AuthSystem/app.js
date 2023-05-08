918b7b8405e2b9397f9b040c7d8c138cd761c7ce
Mohd Ayan Ansari <mohdayanansari1999@gmail.com>
2023-05-08 12:52:13 +0530

    comiit

--- AuthSystem/app.js
+++ AuthSystem/app.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
-const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const app = express();
+
+const User = require("./models/User");
 
 app.use(express.json());
 
@@ -11,16 +14,40 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-  if (!(email && password && firstName && lastName)) {
-    res.status(400).send("All filed are required!");
-  }
-
   try {
+    const { firstName, lastName, email, password } = req.body;
+    if (!(email && password && firstName && lastName)) {
+      res.status(400).send("All filed are required!");
+    }
     const existingUser = await User.findOne({ email });
     if (existingUser) res.status(401).send("User already exists");
+    const myEncPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email: email.toLowerCase(),
+      password: myEncPassword,
+    });
+
+    //token
+    const token = jwt.sign(
+      {
+        user_id: user._id,
+        email,
+      },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    user.token = token;
+
+    //update or not
+    user.password = undefined;
+    res.status(201).json(user);
   } catch (error) {
-    console.log("Error in finding user", error);
+    console.log("Error:", error);
   }
 });
 
